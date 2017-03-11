93ea199fdd75feb3ae45d540178e544deb77efb1
sloveys <sloveys@mail.uoguelph.ca>
2017-03-11 16:24:59 -0500

    Change to single method
    changed object to behave like function

--- src/smart/MarkData.java
+++ src/smart/MarkData.java
@@ -12,24 +12,8 @@ import java.util.ArrayList;
  * @author James
  */
 public class MarkData {
-    private ArrayList<Integer> listOfMarks;
-    private ArrayList<Integer> listOfClassMarks;
-    private int aScore;
-    private int bScore;
-    
-    public ArrayList<Integer> getListOfMarks() {
-        return this.listOfMarks;
+    public SNHList analysis(String file){
+        return null;
     }
     
-    public ArrayList<Integer> getListOfClassMarks() {
-        return this.listOfClassMarks;
-    }
-    
-    public int getAScore() {
-        return this.aScore;
-    }
-    
-    public int getBScore() {
-        return this.bScore;
-    }
 }
