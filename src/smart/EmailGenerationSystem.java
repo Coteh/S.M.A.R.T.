c793be9bcd84edafd2ef8325511eeb0fc984053b
Coteh <james_c_34@live.com>
2017-03-11 16:55:11 -0500

    Start excel reading

--- src/smart/EmailGenerationSystem.java
+++ src/smart/EmailGenerationSystem.java
@@ -5,6 +5,10 @@
  */
 package smart;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author James
@@ -14,7 +18,14 @@ public class EmailGenerationSystem {
     private String helperEmail;
 
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+        if (args.length <= 0) {
+            System.out.println("usage: SMART [WORKBOOK_FILE]");
+            System.exit(1);
+        }
+        try {
+            MarkDataInput.getData(args[0]);
+        } catch (IOException ex) {
+            Logger.getLogger(EmailGenerationSystem.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+        }
     }
 }
--- src/smart/MarkDataInput.java
+++ src/smart/MarkDataInput.java
@@ -5,12 +5,50 @@
  */
 package smart;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
  *
  * @author James
  */
 public class MarkDataInput {
-    MarkData getData(String file) {
-        throw new UnsupportedOperationException();
+    public static MarkData[] getData(String filepath) throws IOException {
+        Workbook excelWorkbook;
+        FileInputStream inputStream = new FileInputStream(new File(filepath));
+        
+        excelWorkbook = new XSSFWorkbook(inputStream);
+        
+        Iterator<Sheet> sheetIterator = excelWorkbook.iterator();
+        
+        //Iterating through each sheet (each sheet denotes a semester)
+        while (sheetIterator.hasNext()) {
+            Sheet sheet = sheetIterator.next();
+            Iterator<Row> rowIterator = sheet.iterator();
+            
+            //Iterating through each row (each row denotes a particular student
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator = row.iterator();
+                
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    Double cellVal = cell.getNumericCellValue();
+                    System.out.println(cellVal);
+                }
+            }
+        }
+        
+        excelWorkbook.close();
+        inputStream.close();
+        
+        return null;
     }
 }
