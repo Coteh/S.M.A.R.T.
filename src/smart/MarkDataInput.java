da254160ae98f58110d0f6446978a47dd69634c0
Coteh <james_c_34@live.com>
2017-03-11 21:19:15 -0500

    We won't need to take in filepath anymore if we're doing mysql

--- src/smart/MarkDataInput.java
+++ src/smart/MarkDataInput.java
@@ -5,6 +5,7 @@
  */
 package smart;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -12,13 +13,13 @@ import java.util.ArrayList;
  * @author James
  */
 public class MarkDataInput {
-    public static ArrayList getStudents(String file) {
+    public static ArrayList getStudents() {
         ArrayList<Student> students = new ArrayList<>();
         //...
         return students;
     }
     
-    public static ArrayList getCourses(String file) {
+    public static ArrayList getCourses() {
         ArrayList<Course> courses = new ArrayList();
         //...
         return courses;
